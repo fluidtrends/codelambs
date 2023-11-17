e1422aa700c13ee905d652d3acf44b51c36e9b37
ManeaEduard98 <eduard@fluidtrends.com>
2023-11-17 17:16:14 +0200

    spinner added on app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './config/router';
+import ReactLoading from 'react-loading'
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    const onPageLoad = () => setIsLoaded(true)
+
+    if (document.readyState === 'complete') {
+      onPageLoad();
+    } else {
+      window.addEventListener('load', onPageLoad, false);
+      return () => window.removeEventListener('load', onPageLoad);
+    }
+  }, []);
+
   return (
-    <RouterProvider router={router} />
+    isLoaded
+      ? <RouterProvider router={router} />
+      : <div className='w-full h-full flex justify-center items-center'>
+        <ReactLoading height='20%' width='20%' className='text-primary' type='spin' />
+      </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ReactLoading from 'react-loading'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Suspense fallback={<ReactLoading height='20%' width='20%' />}>
-      <App />
-    </Suspense>
+    <App />
   </React.StrictMode>,
 )
