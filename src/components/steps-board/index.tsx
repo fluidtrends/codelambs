55e121a2ae7ccfe2f7b9064ba12b40b64a64c3d8
ManeaEduard98 <eduard@fluidtrends.com>
2023-11-27 12:50:02 +0200

    auto scroll on stepboard length changes

--- src/components/steps-board/index.tsx
+++ src/components/steps-board/index.tsx
@@ -1,15 +1,14 @@
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
 import Step from "./Step"
-import { useDraggable } from "react-use-draggable-scroll"
 import useStepsStore from "../../stores/steps";
 import useInputDetailsStore from "../../stores/inputDetails";
 
 const StepsBoard = () => {
 
-	const ref = useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
-	const { events } = useDraggable(ref)
 	const { steps } = useStepsStore()
 	const { number, coordinate } = useInputDetailsStore()
+
+	const ref = useRef()
 
 	const getSteps = () => [...steps, { count: number, direction: coordinate, id: '-1' }]
 		.map((step: any, index: number) => <Step
@@ -17,6 +16,10 @@ const StepsBoard = () => {
 			key={step.id}
 			{...step}
 		/>)
+
+	useEffect(() => {
+		(ref as any)?.current?.scrollIntoView({ behavior: 'smooth' })
+	}, [steps.length])
 
 	return (
 		<div className='h-full overflow-auto relative'>
@@ -26,12 +29,9 @@ const StepsBoard = () => {
 				onContextMenu={event => event.preventDefault()}
 			/>
 			<div className='w-full h-full py-[2vw] z-50 relative'>
-				<div
-					className='flex flex-col gap-[1vw] items-start justify-start overflow-auto h-full px-[2.5vw] w-[17vw]'
-					{...events}
-					ref={ref}
-				>
+				<div className='flex flex-col gap-[1vw] items-start justify-start overflow-auto h-full px-[2.5vw] w-[17vw]'>
 					{getSteps()}
+					<div ref={ref as any} className='h-0' />
 				</div>
 			</div>
 		</div>
